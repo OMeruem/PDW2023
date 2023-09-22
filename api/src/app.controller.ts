c6d8cb4be8bb9d8800bfec3270a8088de0dbd2c3
IdrisTA <Idris2813@hotmail.com>
2023-09-22 19:21:29 +0200

    exceptions done

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get} from '@nestjs/common';
+import {AppService} from './app.service';
+import {ApiResponse} from '@common/api/data';
+import {ApiCodeResponse} from '@common/api/data/enum/api-code.response';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): ApiResponse {
+        return {result:true , code: ApiCodeResponse.TEST, data:null};
+    }
 }
